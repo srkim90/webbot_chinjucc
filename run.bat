4e3a3558e3a0323e767e1bd9e19344ae16aa3460
srkim90 <shshrka001@gmail.com>
2020-11-20 17:58:07 +0900

    modified:   run.bat 	modified:   run_debug.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 cd scripts\chinjucc
+set PATH=%PATH%;..\anaconda3;..\anaconda3\Scripts;..\anaconda3\Library\bin
+set QT_PLUGIN_PATH=..\anaconda3\Library\plugins
 ..\PortableGit\bin\git.exe pull
 ..\anaconda3\pythonw main_chinjucc.py
--- run_debug.bat
+++ run_debug.bat
@@ -1,4 +1,6 @@
 cd scripts\chinjucc
+set PATH=%PATH%;..\anaconda3;..\anaconda3\Scripts;..\anaconda3\Library\bin
+set QT_PLUGIN_PATH=..\anaconda3\Library\plugins
 ..\PortableGit\bin\git.exe pull
 ..\anaconda3\python main_chinjucc.py
 timeout 9999
