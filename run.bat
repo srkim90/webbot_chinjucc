07055dbfb1e1582da14dd18bf23f76dee2e03932
srkim90 <shshrka001@gmail.com>
2020-11-20 10:53:18 +0900

    modified:   run.bat 	modified:   run_debug.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 cd scripts\chinjucc
+..\PortableGit\bin\git.exe pull
 ..\anaconda3\pythonw main_chinjucc.py
--- run_debug.bat
+++ run_debug.bat
@@ -1,3 +1,4 @@
 cd scripts\chinjucc
+..\PortableGit\bin\git.exe pull
 ..\anaconda3\python main_chinjucc.py
 timeout 9999
